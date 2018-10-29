09ad8521c94aa9641bb6684551ee8ceb63fbf47f
Oliver Leite <spiele2002@hotmail.com>
2018-10-29 14:53:53 -0300

    First Commit

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>Gear Creations</title>
+
+      <link rel="stylesheet" href="lib/bootstrap-4.1.3-dist/css/bootstrap.min.css"/>
+      <link rel="stylesheet" href="css/gearc.css"/>
+
+      <script src="lib/jquery-3.3.1.min.js"></script>
+      <script src="lib/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+   </head>
+   <body>
+
+      <div class="header">
+         <object class="gearc-logo" type="image/svg+xml" data="media/svg/GearC Logo Extenso.svg"></object>
+         <div class="navigation">
+
+         </div>
+      </div>
+
+      <div class="carousel">
+         <div class="video-overlay"></div>
+         <video autoplay muted loop id="video-rain" class="carousel-video">
+           <source src="media/video/rain.mp4" type="video/mp4">
+         </video>
+      </div>
