5516eaf5d1d625e3b4388d68f50fe7aa3c7834a5
ViniciusLibano <vinicius2010libano@gmail.com>
2018-10-31 07:35:32 -0300

    Bootstrap q deu erado

--- header.php
+++ header.php
@@ -11,13 +11,37 @@
       <script src="lib/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
    </head>
    <body>
+      <nav class="header navbar row navbar-expand-lg navbar-light">
+         <div class="container">
 
-      <div class="header">
-         <object class="gearc-logo" type="image/svg+xml" data="media/svg/GearC Logo Extenso.svg"></object>
-         <div class="navigation">
+            <div class="navbar-header">
+               <a class="navbar-brand" href="#">
+                 <object class="gearc-logo" type="image/svg+xml" data="media/svg/GearC Logo Extenso.svg"></object>
+               </a>
+            </div>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                 <a class="nav-link" href="#">Projetos</a>
+                </li>
+                <li class="nav-item">
+                 <a class="nav-link" href="#">Contato</a>
+                </li>
+                <li class="nav-item">
+                 <a class="nav-link" href="#">Sobre</a>
+                </li>
+              </ul>
+            </div>
 
          </div>
-      </div>
+      </nav>
+
 
       <div class="carousel">
          <div class="video-overlay"></div>
